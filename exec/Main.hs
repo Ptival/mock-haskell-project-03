3ac4a1654ffcb2a41f8a08b3dc5953d815a6fda5
Valentin Robert <valentin.robert.42@gmail.com>
2019-11-05 10:44:55 -0800

    add unique dependency to library and executable

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import Data.Time.Calendar ( Day )
+
 import ExposedModule
+
+type MyDay = Day
 
 main = print c
--- lib/ExposedModule.hs
+++ lib/ExposedModule.hs
@@ -1,8 +1,11 @@
 module ExposedModule where
 
-import Data.List (or)
+import Data.List   ( or )
+import Data.Map    ( Map )
 
 import OtherModule
+
+type GoodInts = Map Int Bool
 
 c :: Bool
 c = or [a, b, a && b, a || b]
